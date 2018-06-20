368dab436976f8fb233142f0dde392968f8349a6
Riz <mrk_rizwan@hotmail.com>
2018-06-20 16:28:17 -0400

    Update First.py

--- First.py
+++ First.py
@@ -1,3 +1,33 @@
+from selenium import webdriver
+import time
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.common.by import *
+from selenium.webdriver.common.keys import Keys
+user = "###"
+pwd = "###"
+#driver = webdriver.Firefox()
+#driver = webdriver.Ie("C:\\Users\\mrkhan\\Desktop\\IEDriver\\IEDriverServer.exe")
+#driver = webdriver.Chrome()
+driver = webdriver.Chrome()
+driver.get("http://###:###@###/###/")
+
+#assert "Google" in driver.title
+#elem = driver.find_element_by_id("buttonCancel")
+#driver.findElement(By.cssSelector("a[href*='long']")).click();
+#driver.find_element(By.L("navTourButtonText navTourSmallText navTourWhiteText")).click()
+#driver.find_element(By.LINK_TEXT("No, ")).click();
+#if(driver.switch_to().frame(driver.find_element(By.ID("InlineDialog_Iframe")))):
+ #   print ('true')
+#driver.find_element(By.CLASS_NAME("navTourCloseButton")).click()
+#driver.switch_to().defaultContent()
+#button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'buttonClose')));
+#button.click();
+
 #This basically switches to the inner Frame if it exists with that name
 if(wait(driver, 4).until(EC.frame_to_be_available_and_switch_to_it("InlineDialog_Iframe"))):
     a = wait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it("InlineDialog_Iframe"))
@@ -7,19 +37,16 @@ driver.switch_to.default_content()
 
 ab = driver.find_element_by_xpath('//*[@id="navTabGroupDiv"]/span[3]')
 ab.click()
-print ('Success x1')
 element = driver.find_element_by_xpath('//*[@id="Tabnav_Project"]')
 
 #Makes sure that you are paying attention
 numb = input("enter 1 to continue? ")
 if(numb=='1'):
     if(driver.find_element_by_xpath('//*[@id="Tabnav_Project"]/a')):
-        print('inside x2')
         a1 = driver.find_element_by_xpath('//*[@id="Tabnav_Project"]/a')
         a1.click()
         a2 = driver.find_element_by_xpath('//*[@id="Tabnav_Project"]/a')
         a2.click()
-        print ('Success x2')
 
 
 numb = input("enter 1 to continue to utility? ")
@@ -28,7 +55,6 @@ if (numb == '1'): ## MAKE SURE YOU MOVE TO UTILITY ACCOUNT MANUALLY
         #driver.switch_to.frame("contentIFrame0")
         driver.switch_to.default_content()
         wait(driver, 4).until(EC.frame_to_be_available_and_switch_to_it("contentIFrame0"))
-        print ('I see it')
         #driver.switch_to.frame(driver.find_element_by_id("contentIFrame0"))
         #wait(driver, 4).until(EC.frame_to_be_available_and_switch_to_it("contentIFrame0"))
         #wait.until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="contentIFrame0"]')))
@@ -40,12 +66,9 @@ if (numb == '1'): ## MAKE SURE YOU MOVE TO UTILITY ACCOUNT MANUALLY
         searchbarclick2 = driver.find_element_by_id("crmGrid_findCriteriaButton")
         searchbarclick2.click()
         time.sleep(5)
-        print ('aaaaaaaaaa')
         searchbarclick = driver.find_element_by_xpath('//*[@id="crmQuickFindTD"]/table')
         searchbarclick.click()
-        print ('successs x3')
         #inputElement = driver.find_element_by_xpath('//*[@id="crmGrid_quickFindContainer"]')
-        print ('successs x4')
         #This paste in the number into the form
         ################################
         ##########
@@ -54,8 +77,18 @@ if (numb == '1'): ## MAKE SURE YOU MOVE TO UTILITY ACCOUNT MANUALLY
         searchbarclick3 = driver.find_element_by_xpath('//*[@id="crmGrid_findCriteria"]')
         searchbarclick3.send_keys('6739540306')
         #searchbarclick.submit()
-        print ('successs x5')
+        searchbarclick2 = driver.find_element_by_id("crmGrid_findCriteriaButton")
+        searchbarclick2.click()
+        selAll = driver.find_element_by_xpath('//*[@id="crmGrid_gridBodyTable_checkBox_Image_All"]')
+        selAll.click()
 
+
+print ('outside x1')
+driver.switch_to.default_content()
+print ('Success x12')
+tre = driver.find_element_by_xpath('//*[@id="trc_utilityaccount|NoRelationship|HomePageGrid|Mscrm.HomepageGrid.trc_utilityaccount.Edit"]/span/a/img')
+print ('Success x2')
+#edit.click()
 
 print ('*******************')
 
