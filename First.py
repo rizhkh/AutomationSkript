7b4b05c611eef0609c20ab38bbb9f999e21d1dce
Riz <mrk_rizwan@hotmail.com>
2018-06-22 15:12:00 -0400

    Update First.py

--- First.py
+++ First.py
@@ -17,6 +17,7 @@ driver = webdriver.Chrome()
 driver.get("http://###:###@###/###/")
 numbers = ['403852699996','2669930001']
 
+
 if(wait(driver, 4).until(EC.frame_to_be_available_and_switch_to_it("InlineDialog_Iframe"))):
     a = wait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it("InlineDialog_Iframe"))
     a=driver.find_element_by_id('buttonClose') #now it looks for the id if it exists clicks it which closes the popup
@@ -45,19 +46,25 @@ if (numb == '1'): ## MAKE SURE YOU MOVE TO UTILITY ACCOUNT MANUALLY
     #Problem: runs in the first try having trouble in the second try its about the xpath
 
     for i in range(len(numbers)):
-        #ki
-        if (wait(driver, 4).until(EC.frame_to_be_available_and_switch_to_it("contentIFrame0"))):
+        print (i)
+        print(numbers[i])
+        if (wait(driver, 4).until(EC.frame_to_be_available_and_switch_to_it("contentIFrame1"))):
+            print ('true')
             driver.switch_to.default_content()
-            wait(driver, 4).until(EC.frame_to_be_available_and_switch_to_it("contentIFrame0"))
-            searchbarclick = driver.find_element_by_id('crmGrid_SavedNewQuerySelector')
-            driver.switch_to.default_content()
+            print('true2')
             #Goes to the clickable frame #############<------
             wait(driver, 4).until(EC.frame_to_be_available_and_switch_to_it("contentIFrame1")) #all clickables are in frame1
+            print('true3')
+            #print('frame 1')
             #Clicks the search bar to enter something
             searchbarclick2 = driver.find_element_by_id("crmGrid_findCriteriaButton")
+            print('true4')
             searchbarclick2.click()
+            print('true5')
             time.sleep(5)
+            print('true6')
             searchbarclick = driver.find_element_by_xpath('//*[@id="crmQuickFindTD"]/table')
+            print('true7')
             searchbarclick.click()
             #This paste in the number into the form
             ################################
@@ -71,35 +78,70 @@ if (numb == '1'): ## MAKE SURE YOU MOVE TO UTILITY ACCOUNT MANUALLY
             if(driver.find_element_by_xpath('//*[@id="crmGrid_gridBodyTable_checkBox_Image_All"]')):
                 selAll = driver.find_element_by_xpath('//*[@id="crmGrid_gridBodyTable_checkBox_Image_All"]')
                 selAll.click()
-                #IF YOU HAVE ANY PROBLEMS ITS PROBABLY RIGHT HERE PROBLEM OPENING UP EDIT
+
+                #This exception throw basically checks if record is more than 1 exception is thrown and it understands more records are present so it runs except part
+                try:
+                    if (driver.find_element_by_xpath('//*[@totalrecordcount="1"]')):
+                        print("***************************")
+                        print("***************************")
+                        print("***************************")
+                        print("***************************")
+                        print("***************************")
+                        print("***************************")
+                        time.sleep(5)
+                        driver.switch_to.default_content()
+                        continue
+
+
+                except:
+                    #IF YOU HAVE ANY PROBLEMS ITS PROBABLY RIGHT HERE PROBLEM OPENING UP EDIT
                 ## MAKES SURE EDIT BUTTON IS VISIBLE
-                driver.switch_to.default_content()
-                time.sleep(2)  ## Try this without sleep
-                ## THIS PRESSED THE EDIT BUTTON
-                if(driver.find_element_by_xpath("//*[@id='trc_utilityaccount|NoRelationship|HomePageGrid|Mscrm.HomepageGrid.trc_utilityaccount.Edit']/span/a/span")):
-                    editbuttonclick = driver.find_element_by_xpath("//*[@id='trc_utilityaccount|NoRelationship|HomePageGrid|Mscrm.HomepageGrid.trc_utilityaccount.Edit']/span/a/span")
-                    hover = ActionChains(driver).move_to_element(editbuttonclick)
-                    hover.perform()
-                    editbuttonclick.click()
+                    driver.switch_to.default_content()
+                    time.sleep(2)  ## Try this without sleep
+                    ## THIS PRESSES THE EDIT BUTTON
+                    if(driver.find_element_by_xpath("//*[@id='trc_utilityaccount|NoRelationship|HomePageGrid|Mscrm.HomepageGrid.trc_utilityaccount.Edit']/span/a/span")):
+                        editbuttonclick = driver.find_element_by_xpath("//*[@id='trc_utilityaccount|NoRelationship|HomePageGrid|Mscrm.HomepageGrid.trc_utilityaccount.Edit']/span/a/span")
+                        hover = ActionChains(driver).move_to_element(editbuttonclick)
+                        hover.perform()
+                        editbuttonclick.click()
+
+                    else:
+                        print ('DOES NOT EXIST')
+                        time.sleep(30)
+                        numb2 = input("enter 9 to exit? ")
+                        if (numb2 == '9'):
+                            driver.close()
+
 
             # moves to the new window or popup
             window_after = driver.window_handles[1]
             driver.switch_to.window(window_after)
-            print('success')
+            #print('success')
 
+            #if does not exist handle t2o code to bring it back and search again
 
             if(driver.find_element_by_xpath( "//*[@id='DlgHdTitle']" )):
                 MTClick = driver.find_element_by_xpath( "// *[ @ id = 'trc_mergedto_lookupTable'] / tbody / tr / td[1] / div" )
                 MTClick.click()
+                #print ('in new window')
+                time.sleep(2)
                 MTClick2 = driver.find_element_by_xpath( "//*[@id='trc_mergedto_ledit']" ) # inputs the search bar
                 MTClick2.send_keys(numbers[i], Keys.ENTER)
+                time.sleep(2)
                 if(driver.find_element_by_class_name( "ms-crm-IL-MenuItem-MoreInfoItem" )):
                     MTClick3 = driver.find_element_by_class_name("ms-crm-IL-MenuItem-MoreInfoItem")
                     MTClick3.click()
+                print ('about to confirm and close new window')
                 ConfrimClick = driver.find_element_by_id("butBegin")
                 ConfrimClick.click()
+
+        #"// *[ @ id = 'navBar']"
+        #driver.switch_to.window(window_before)
+        time.sleep(20)
         driver.switch_to.window(window_before)
-        time.sleep(15)
+        if ((driver.find_element_by_id("navBar"))):
+            print ("It switched back")
+
 
 ##### AT THIS POINT MERGING IS DONE AND ITS BACK IN THE MAIN WINDOW
 
@@ -113,4 +155,5 @@ print ('*******************')
 
 
 #driver.close()
+
 
